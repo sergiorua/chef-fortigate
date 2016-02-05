54694f2dd1ce19556dd66b419967a9bbc07db0a5
Sergio Rua <sergio.rua@skybettingandgaming.com>
2016-02-05 13:41:10 +0000

    bump version to 1.0.18

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'github@rua.me.uk'
 license          'All rights reserved'
 description      'Installs/Configures fortigate'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.17'
+version          '1.0.18'
